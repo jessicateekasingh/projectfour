cf61f9394f4689114863d34285131b624cc05eb1
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-07-19 15:43:23 -0400

    added code to controllers/pow.php

--- controllers/pow.php
+++ controllers/pow.php
@@ -1,4 +1,31 @@
 <?php
-include_once __DIR__ . '/../models/pows.php';
+include_once __DIR__ . '/../models/powr.php';
+header('Content-Type: application/json');
+if ($_REQUEST['action'] === 'index') {
+  echo json_encode(Posts::all());
+} else if ($_REQUEST['action'] === 'post') {
+    $request_body = file_get_contents('php://input');
+    $body_object = json_decode($request_body);
+    $new_post = new Post(null,
+        $body_object->title,
+        $body_object->author,
+        $body_object->content);
+    $all_posts = Posts::create($new_post);
+    echo json_encode($all_posts);
 
+} else if ($_REQUEST['action'] === 'update'){
+    $request_body = file_get_contents('php://input');
+    $body_object = json_decode($request_body);
+    $updated_post = new Post($_REQUEST['id'], $body_object->title, $body_object->author, $body_object->content);
+    $all_posts = Posts::update($updated_post);
+    echo json_encode($all_posts);
+
+} else if ($_REQUEST['action'] === 'delete'){
+    $all_posts = Posts::delete($_REQUEST['id']);
+    echo json_encode($all_posts);
+
+} else if ($_REQUEST['action'] === 'single'){
+    $post = Posts::single($_REQUEST ['id']);
+    echo json_encode($post);
+}
 ?>
